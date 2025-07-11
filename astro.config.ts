0d54be3637f44b620bcc4ea7c09f3ba54d44d195
Joseph Muller <jtmuller5@gmail.com>
2025-07-11 09:13:58 -0400

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://joemuller.com',
-	base: 'jtmuller5.github.io',
 	integrations: [mdx(), sitemap()],
 	markdown: {
 		shikiConfig: {
